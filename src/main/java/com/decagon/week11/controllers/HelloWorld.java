bf0e6b88bc6ccf78aab79e50e906faa161a41814
aliyura <e.rabiualiyu@gmail.com>
2022-12-14 15:43:01 +0100

    Final release

---
+++ src/main/java/com/decagon/week11/controllers/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.decagon.week11.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "This is hello from our dockerized app";
+    }
+
+    @GetMapping("thanks")
+    public String sayThanks(){
+        return "Thank you";
+    }
+
+}
